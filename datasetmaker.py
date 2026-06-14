d7997689cc8208d2654a7d4e163ea7971d955598
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2026-06-14 18:37:00 -0500

    forgot save

--- datasetmaker.py
+++ datasetmaker.py
@@ -6,11 +6,11 @@ data = []
 airfoil_gen = unifiedairfoilgenerator()
 attempts = 0
 while len(data) < 10000:
-    max_camber = round(random.uniform(0, 0.1), 3)
-    camber_pos = round(random.uniform(0.1, 0.8),3)
-    thickness = round(random.uniform(0.07, 0.2),3)
-    reynolds = random.randint(100000, 1000000)
-    aoa = round(random.uniform(-5, 15), 1)
+    max_camber = round(random.uniform(0, 0.05), 3)
+    camber_pos = round(random.uniform(0.2, 0.7),3)
+    thickness = round(random.uniform(0.08, 0.15),3)
+    reynolds = random.randint(300000, 1000000)
+    aoa = round(random.uniform(-4, 8), 1)
     attempts += 1
     try:
         x, y = airfoil_gen.generate_airfoil(max_camber, camber_pos, thickness)
